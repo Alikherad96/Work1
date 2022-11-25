a78b6e4cbd8fc9789404f4387894042bb74e30dd
Alikherad96 <118910296+Alikherad96@users.noreply.github.com>
2022-11-25 17:49:49 +0330

    Add files via upload

--- calc .py
+++ calc .py
@@ -1,9 +1,9 @@
 
-print ("welcome to my clculator")
+print("welcome to my clculator")
 
 import math
 
-pi= (math.pi)
+pi = (math.pi)
 print(" + : sum ")
 print(" - : sub ")
 print(" * : mul ")
@@ -16,42 +16,53 @@ print("t : tan")
 print("cot : cot")
 print("f : factorial")
 
+while 2 > 1:
+    op = input("select ur op : ")
+    if op == "exit":
+        print("az hamrayiye shoma motshakeram")
+        break
+    else:
+        if op == "+" or op == "-" or op == "*" or op == "/" or op == "s" or op == "l" or op == "sq" or op == "c" or op == "t" or op == "cot" or op == "f":
 
-op=input()
+            a = float(input(" first num :"))
+            if op == "+" or op == "-" or op == "*" or op == "/":
+               b = float(input("second num :"))
 
-if op == "+" or op == "-" or op == "*" or op == "/" or op == "s" or op == "l" or op == "sq" or op=="c" or op == "t" or op=="cot" or op=="f" :
-
-    a = float(input(" first num :"))
-    if op == "+" or op == "-" or op=="*" or op=="/" :
-       b=float(input("second num :"))
-
-    if op == "+":
-        r = a+b
-    elif op == "-":
-        r = a-b
-    elif op == "*":
-        r = a*b
-    elif op == "/":
-        if b == 0:
-            r = ("cnt div by 0")
-        else :
-            r = a/b
-    elif op == "s":
-        a1 = (a*pi)/ 180
-        r = math.sin(a1)
-    elif op == "l" :
-        r = math.log(a)
-    elif op == "sq" :
-        r = math.sqrt(a)
-    elif op == "c" :
-        r = math.cos((a*pi)/ 180)
-    elif op == "t" :
-        r = math.tan((a*pi)/ 180)
-    elif op == "cot" :
-        r = 1/(math.tan((a * pi) / 180))
-    elif op == "f" :
-        r = math.factorial(int(a))
-
-    print(r)
-else:
-    print ("Oh NO")
+            if op == "+":
+                r = a+b
+            elif op == "-":
+                r = a-b
+            elif op == "*":
+                r = a*b
+            elif op == "/":
+                if b == 0:
+                    r = ("can't div by 0")
+                else:
+                    r = a/b
+            elif op == "s":
+                a1 = (a*pi) / 180
+                r = math.sin(a1)
+            elif op == "l":
+                if a > 0:
+                    r = math.log(a,10)
+                else:
+                    r = ("can't do this")
+            elif op == "sq":
+                if a >= 0:
+                    r = math.sqrt(a)
+                else:
+                    r = ("can't do this")
+            elif op == "c":
+                r = math.cos((a*pi) / 180)
+            elif op == "t":
+                r = math.tan((a*pi) / 180)
+            elif op == "cot":
+                if a != 0:
+                    r = 1 / (math.tan((a * pi) / 180))
+                else:
+                    r = ("can't do this")
+            elif op == "f":
+                r = math.factorial(int(a))
+            print(r)
+        else:
+            print("Oh NO")
--- mosallas.py
+++ mosallas.py
@@ -1,10 +1,17 @@
-print ("plz intr 3 zele of mosalas")
+print("plz intr 3 zele of mosalas")
+print("baraye kharej shodan : exit ")
+while 2 > 1:
+    aa = input("1st : ")
+    if aa == "exit":
+        print("hope u enjoy")
+        break
+    else:
 
-a=float(input("1st"))
-b=float(input("2nd"))
-c=float(input("3rd"))
+        a = float(aa)
+        b = float(input("2nd : "))
+        c = float(input("3rd : "))
 
-if a< b+c and b < a+c and c< a+b :
-    print ("in mosalas ghabele rasm ast")
-else :
-    print ("in mosalas niiiiiiiiiiist")
+        if a < b+c and b < a+c and c < a+b:
+            print("in mosalas ghabele rasm ast")
+        else:
+            print("in mosalas niiiiiiiiiiist")
